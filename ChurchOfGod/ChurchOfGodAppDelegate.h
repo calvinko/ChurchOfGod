10554f4681c036f12ecb34f0f1b66e8edc4d12f7
Calvin Ko <ko@imac1.gateway.2wire.net>
2011-03-11 23:24:20 -0800

    Initial Commit

---
+++ ChurchOfGod/ChurchOfGodAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ChurchOfGodAppDelegate.h
+//  ChurchOfGod
+//
+//  Created by Calvin Ko on 3/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChurchOfGodAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
